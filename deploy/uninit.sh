ab7aa3e495532f9e699d47020f4e0fa118fb8030
democat <cwongmath@gmail.com>
2025-02-22 02:54:14 -0600

    club-manager, not clubManager

--- deploy/uninit.sh
+++ deploy/uninit.sh
@@ -1,1 +1,1 @@
-rm /etc/systemd/system/gunicorn.service /etc/systemd/system/gunicorn.socket /etc/systemd/system/discord_bot.service /etc/nginx/sites-available/clubManager /etc/nginx/sites-enabled/clubManager
+rm /etc/systemd/system/gunicorn.service /etc/systemd/system/gunicorn.socket /etc/systemd/system/discord_bot.service /etc/nginx/sites-available/club-manager /etc/nginx/sites-enabled/club-manager
